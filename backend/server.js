e50c057f19a6ff921d0a190769b55c0ee82d6e15
Davie555 <davidkivilu337@gmail.com>
2026-03-18 18:52:22 +0300

    service.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ connectDB()
 /* 🛠️ Middleware Stack */
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
